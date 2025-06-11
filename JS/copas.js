20685779045e3918d8f9e80e97576c1fc5d6ecd0
Kevin Silva <kevinsilva.iar21@gmail.com>
2025-06-11 14:05:11 -0300

    JS finalizado das respostas

--- JS/copas.js
+++ JS/copas.js
@@ -18,6 +18,25 @@
   });
 
   //-------------PEGAR OS DADOS CERTOS X ERRADOS
-  
+  document.getElementById('formRadio').addEventListener('submit', function(event) {
+  event.preventDefault();
 
- 
+const respostas = [];
+const totalPerguntas = 10;
+
+for (let i = 1; i <= totalPerguntas; i++) {
+    const selecionado = document.querySelector(`input[name="pq${i}"]:checked`);
+    if (selecionado) {
+      respostas.push(selecionado.value);
+    } else {
+      respostas.push(null);
+    }
+  }
+console.log(respostas);
+
+  const acertos = respostas.filter(letra => letra === 'c').length;
+
+  console.log(acertos);
+
+});
+
